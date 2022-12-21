c0db3249821b2ccc828e3bbbd4d4724ff60fe0cd
NicolasAyal <javier17utn@gmail.com>
2022-12-21 14:25:21 -0300

    gallery-img-finish

---
+++ src/Components/Accordion.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import './accordion.scss'
+
+const accordion = () => {
+  return (
+    <>
+    <div className="accordion">
+        <span className="img"></span>
+        <span className="img"></span>
+        <span className="img"></span>
+        <span className="img"></span>
+        <span className="img"></span>
+    </div>
+    </>
+  )
+}
+
+export default accordion
